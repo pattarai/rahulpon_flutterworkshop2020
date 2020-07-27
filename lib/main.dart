8280ea9713e5cbf0c4abdd2427fad6a253a65157
Rahul Pon <rahwinside@gmail.com>
2020-07-27 11:49:05 +0530

    stack 2

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,10 @@ class Stack extends StatefulWidget {
 }
 
 class _Stack extends State<Stack> {
+  TextEditingController _stackController = new TextEditingController();
+
+  List<int> stack = new List<int>();
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -25,29 +29,43 @@ class _Stack extends State<Stack> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-             Text(
+            Text(
               "Enter a number to push into the stack:",
-              textAlign: TextAlign.center,),
-            TextFormField(),
-            Padding(padding: EdgeInsets.all(10),),
+              textAlign: TextAlign.center,
+            ),
+            TextFormField(
+              controller: _stackController,
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 RaisedButton(
-                  child: Text("Push"),
-                  onPressed: null,
+                    child: Text("Push"),
+                    onPressed: () {
+                      stack.add(int.parse(_stackController.text));
+                      print(stack.toString());
+                    }),
+                Padding(
+                  padding: EdgeInsets.all(10),
                 ),
-                Padding(padding: EdgeInsets.all(10),),
                 RaisedButton(
-                  child: Text("Pop"),
-                  onPressed: null,
-                ),
+                    child: Text("Pop"),
+                    onPressed: () {
+                      stack.removeLast();
+                      print(stack.toString());
+                    }),
               ],
             ),
-            Padding(padding: EdgeInsets.all(10),),
+            Padding(
+              padding: EdgeInsets.all(10),
+            ),
             Text(
               "Elements in the stack are:",
-              textAlign: TextAlign.center,),
+              textAlign: TextAlign.center,
+            ),
           ],
         ),
       ),
